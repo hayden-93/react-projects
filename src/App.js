94e5fa36bb050343fc5a34e9d8f21b556878086c
Hayden Buckley-Smith <hayden@abstack.co>
2023-05-13 17:50:04 +0100

    feat: create Profile component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
-import Header from "./components/Header";
 import "./App.css";
 import logo from "./assets/logo.svg";
+
+import Profile from "./pages/Profile";
+import Header from "./components/Header";
 
 function App() {
   return (
     <div className="App">
       <Header logo={logo} />
+      <Profile userName="octocat" />
     </div>
   );
 }
