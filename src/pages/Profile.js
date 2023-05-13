21d8c214c854875940ff6269f2c1ead6c0c4206e
Hayden Buckley-Smith <hayden@abstack.co>
2023-05-13 18:14:30 +0100

    feat: add list items from the github API and styling

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -6,7 +6,7 @@ function Profile({ userName }) {
 
   useEffect(() => {
     async function fetchData() {
-      const profile = await fetch("https://api.github.com/users/${userName}");
+      const profile = await fetch(`https://api.github.com/users/${userName}`);
       const result = await profile.json();
       if (result) {
         setProfile(result);
@@ -19,7 +19,42 @@ function Profile({ userName }) {
   return (
     <div>
       <h2>About me</h2>
-      {loading ? <span>Loading...</span> : <ul></ul>}
+      {loading ? (
+        <span>Loading...</span>
+      ) : (
+        <ul>
+          <li>
+            <span>avatar_url: </span>
+            {profile.avatar_url}
+          </li>
+          <li>
+            <span>html_url: </span>
+            {profile.html_url}
+          </li>
+          <li>
+            <span>repos_url: </span>
+            {profile.repos_url}
+          </li>
+          <li>
+            <span>name: </span> {profile.name}
+          </li>
+          <li>
+            <span>company: </span>
+            {profile.company}
+          </li>
+          <li>
+            <span>location: </span>
+            {profile.location}
+          </li>
+          <li>
+            <span>email: </span>
+            {profile.email}
+          </li>
+          <li>
+            <span>bio: </span> {profile.bio}
+          </li>
+        </ul>
+      )}
     </div>
   );
 }
