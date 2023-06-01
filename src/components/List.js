b364d8f553136613741a3671fcfb5dc9a9a590ff
Hayden Buckley-Smith <hayden@abstack.co>
2023-06-01 14:00:53 +0100

    refactor: List component

--- src/components/List.js
+++ src/components/List.js
@@ -1,10 +1,13 @@
+import { Link } from "react-router-dom";
+
 function List({ items }) {
   return (
     <ul>
-      {items.map((item) => (
-        <li key={item.field}>
-          <span>{item.field}: </span>
-          {item.value}
+      {items.map(({ field, url }) => (
+        <li key={field}>
+          <span>
+            {field}: <Link to={url}>{url}</Link>
+          </span>
         </li>
       ))}
     </ul>
