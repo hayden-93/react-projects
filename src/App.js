fbcf1f0cca662e8ecafecc25d4bab22812c38b19
Hayden Buckley-Smith <hayden@abstack.co>
2023-06-02 14:07:45 +0100

    refactor: replace styling to use styled components

--- src/App.js
+++ src/App.js
@@ -1,15 +1,32 @@
-import "./App.css";
+import styled, { createGlobalStyle } from "styled-components";
+
 import { Backlog } from "./Pages/Backlog";
 import { Board } from "./Pages/Board/Board";
 import { Header } from "./components/Header/Header";
 
+const GlobalStyle = createGlobalStyle`
+  body {
+    margin: 0;
+    font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', 'Roboto', 'Oxygen', 'Ubuntu', 'Cantarell', 'Fira Sans', 'Droid Sans', 'Helvetica Neue', sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+  }
+`;
+
+const AppWrapper = styled.div`
+  text-align: center;
+`;
+
 function App() {
   return (
-    <div className="App">
-      <Header />
-      <Board />
-      <Backlog />
-    </div>
+    <>
+      <GlobalStyle />
+      <AppWrapper>
+        <Header />
+        <Board />
+        <Backlog />
+      </AppWrapper>
+    </>
   );
 }
 
--- src/components/Lane/Lane.js
+++ src/components/Lane/Lane.js
@@ -1,5 +1,17 @@
-import "./Lane.css";
+import styled from "styled-components";
 import { Task } from "../Task/Task";
+
+const LaneWrapper = styled.div`
+  text-align: left;
+  padding: 0;
+  background: lightGray;
+  border-radius: 20px;
+  min-height: 50vh;
+  width: 20vw;
+  @media (max-width: 768px) {
+    margin-bottom: 5%;
+  }
+`;
 
 export function Lane({
   laneId,
@@ -12,11 +24,7 @@ export function Lane({
   onDrop,
 }) {
   return (
-    <div
-      className="Lane-wrapper"
-      onDragOver={onDragOver}
-      onDrop={(e) => onDrop(e, laneId)}
-    >
+    <LaneWrapper onDragOver={onDragOver} onDrop={(e) => onDrop(e, laneId)}>
       <h2>{title}</h2>
       {loading || error ? (
         <span>{error || "Loading..."}</span>
@@ -31,6 +39,6 @@ export function Lane({
           />
         ))
       )}
-    </div>
+    </LaneWrapper>
   );
 }
