e206bc00e9da7b1636010e67edc19498c2e06555
Hayden Buckley-Smith <hayden@abstack.co>
2023-06-05 15:11:35 +0100

    feat: add local state variables to fetch data from API

--- src/app/questions/page.tsx
+++ src/app/questions/page.tsx
@@ -1,6 +1,8 @@
 "use client";
 
+import { useState, useEffect } from "react";
 import styled from "styled-components";
+import { Card } from "../components/Card/Card";
 
 const QuestionsContainer = styled.div`
   display: flex;
@@ -10,6 +12,24 @@ const QuestionsContainer = styled.div`
 `;
 
 export default function QuestionsPage() {
+  const [questions, setQuestions] = useState([]);
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    async function fetchData() {
+      const data = await fetch(
+        "https://api.stackexchange.com/2.2/questions?order=desc&sort=hot&tagged=reactjs&site=stackoverflow"
+      );
+      const result = await data.json();
+
+      if (result) {
+        setQuestions(result.items);
+        setLoading(false);
+      }
+    }
+    fetchData();
+  }, []);
+
   return (
     <QuestionsContainer>
       <h2>Questions</h2>
