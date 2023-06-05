71b4be51d9341d58a52cc6e56d87418ca510ed84
Hayden Buckley-Smith <hayden@abstack.co>
2023-06-05 12:09:27 +0100

    feat: add styling to QuestionsContainer

--- src/app/questions/questions.tsx
+++ src/app/questions/questions.tsx
@@ -1,9 +1,16 @@
 import styled from "styled-components";
 
+const QuestionsContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  margin: 5%;
+`;
+
 export function Questions() {
   return (
-    <>
-      <></>
-    </>
+    <QuestionsContainer>
+      <h2>Questions</h2>
+    </QuestionsContainer>
   );
 }
