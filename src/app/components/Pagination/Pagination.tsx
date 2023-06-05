617a7cfcd8f4d66c62766f2fd1fe451b1ac73883
Hayden Buckley-Smith <hayden@abstack.co>
2023-06-05 17:10:51 +0100

    feat: create Pagination component

---
+++ src/app/components/Pagination/Pagination.tsx
@@ -0,0 +1,20 @@
+import styled from "styled-components";
+import Link from "next/link";
+
+const PaginationLink = styled.div`
+  padding: 2%;
+  margin: 1%;
+  background: orange;
+  cursor: pointer;
+  color: white;
+  text-decoration: none;
+  border-radius: 5px;
+`;
+
+export function Pagination() {
+  return (
+    <>
+      <></>
+    </>
+  );
+}
